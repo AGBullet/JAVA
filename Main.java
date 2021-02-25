a58a307053133904d4e1cb8c318d73ac49e344d2
AGBullet <denism3melnikov@list.ru>
2021-02-25 20:51:03 +0300

    HomeWork 5

--- Main.java
+++ Main.java
@@ -1,142 +1,20 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
-    static char[][] map;
-    static final int SIZE = 3;
 
-    static final char DOT_EMPTY ='.';
-    static final char DOT_X='X';
-    static final char DOT_O='O';
+    public static void main(String[] args) {
 
-    public static void main(String[] args)
-    {
-        initMap();
-        printMap();
-        while (true)
-        {
-            humanTurn();
-            printMap();
-            if (checkWin(DOT_X))
-            {
-                System.out.println("Победил человек");
-                break;
-            }
-            if (isMapFull())
-            {
-                System.out.println("Ничья");
-                break;
-            }
-            aiTurn();
-            printMap();
-            if (checkWin(DOT_O))
-            {
-                System.out.println("Победил компьютер");
-                break;
-            }
-            if (isMapFull())
-            {
-                System.out.println("Ничья");
-                break;
-            }
-        }
-        System.out.println("Игра окончена.");
+	Employee[] arr= new Employee[5];
+	arr[0]= new Employee("Иванов Денис","Инженер1","sdfwreg@mail.ru","89261112233",30000,30);
+	arr[1]= new Employee("Иванов Иван","Инженер2","123sdf@mail.ru","892611134546",31000,19);
+	arr[2]= new Employee("Иванов Сергей","Инженер3","ytrewq@mail.ru","89261145657",22000,28);
+	arr[3]= new Employee("Иванов Михаил","Инженер4","qwerty@mail.ru","89261112123",36000,45);
+	arr[4]= new Employee("Иванов Эдуард","Инженер5","sdfaq1@mail.ru","89261111111",50000,55);
 
-    }
-    static void initMap()
-    {
-        map = new char[SIZE][SIZE];
-        for(int i=0; i <SIZE; i++)
-        {
-            for (int j = 0; j<SIZE;j++)
-            {
-                map[i][j]=DOT_EMPTY;
-            }
-        }
-    }
-    static void printMap()
-    {
-        for (int i = 0;i<=SIZE; i++ )
-        {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i =0; i <SIZE; i++)
-        {
-            System.out.print((i+1) + " ");
-            for(int j=0;j<SIZE;j++)
-            {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    static void humanTurn()
-    {
-        Scanner sc = new Scanner(System.in);
-        int x;
-        int y;
-        do
-            {
-                System.out.println("Введите координаты в формате X Y");
-                x=sc.nextInt()-1;
-                y=sc.nextInt()-1;
+	for(Employee A : arr)
+	{
+		if(A.getAge()>40) A.show();
 
-            }
-        while (!isCellValid(x,y));
-        map[y][x]= DOT_X;
+	}
 
 
-    }
-    static void aiTurn()
-    {
-        Random sc = new Random();
-        int x;
-        int y;
-        do
-        {
-
-            x=sc.nextInt(SIZE);
-            y=sc.nextInt(SIZE);
-
-        }
-        while (!isCellValid(x,y));
-        System.out.println("Компьютер сходил в точку " + (x+1) + " "+ (y+1));
-        map[y][x]= DOT_O;
-
-
-    }
-    static boolean isCellValid(int x,int y)
-    {
-        if (x < 0 || x>=SIZE || y<0 || y >= SIZE)
-        {
-            return false;
-        }
-        if (map[y][x] == DOT_EMPTY)
-        {
-            return true;
-        }
-        return false;
-    }
-    static boolean checkWin(char symb)
-    {
-        for(int i = 0; i<3;i++)
-        {
-            if((map[i][0]== symb&& map[i][1]== symb && map[i][2]==symb)||(map[0][i]== symb&& map[1][i]== symb && map[2][i]==symb)) return true;
-            if((map[0][0]== symb&& map[1][1]== symb && map[2][2]==symb)||(map[2][0]== symb&& map[1][1]== symb && map[0][2]==symb)) return true;
-        }
-        return false;
-
-    }
-    static boolean isMapFull()
-    {
-        for(int i=0;i<SIZE;i++)
-        {
-            for(int j=0; j<SIZE; j++)
-            {
-                if(map[i][j]== DOT_EMPTY) return false;
-            }
-        }
-        return true;
     }
 }
